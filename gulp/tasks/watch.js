e1668915f372c5cc9acd4028cd316a3de51b605b
alexrewrew <alexrewrew@live.com>
2018-02-28 14:14:48 +0200

    Update 28.02

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,6 +2,7 @@ module.exports = function () {
     $.gulp.task('watch', function () {
         $.gulp.watch('app/pages/**/*.pug', $.gulp.series('pug'));
         $.gulp.watch('app/stylus/**/*.styl', $.gulp.series('stylus'));
+        $.gulp.watch('app/source/**/*.styl', $.gulp.series('stylus'));
         $.gulp.watch('app/scripts/**/*.js', $.gulp.series('scripts'));
     });
 }
