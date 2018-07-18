6a8a6136bcf8c307d61f37a38e0649963850f9c0
alexrewrew <alexrewrew@live.com>
2018-07-18 11:00:39 +0300

    changes

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -88,7 +88,55 @@
         DOMs.menuTrigger.click(function (e) {
             e.preventDefault();
             DOMs.documentBodyHtml.toggleClass("open");
+
+            if ($('body').hasClass('open')) {
+                disableScroll();
+            } else {
+                enableScroll();
+            }
         });
+
+        var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+        function preventDefault(e) {
+            e = e || window.event;
+            if (e.preventDefault) {
+                e.preventDefault();
+            }
+            e.returnValue = false;
+        }
+
+        function preventDefaultForScrollKeys(e) {
+            if (keys[e.keyCode]) {
+                preventDefault(e);
+                return false;
+            }
+        }
+
+        function disableScroll() {
+            if (window.matchMedia("(max-width: 767px)").matches) {
+                if (window.addEventListener) {
+                    // older FF
+                    window.addEventListener('DOMMouseScroll', preventDefault, false);
+                }
+                window.onwheel = preventDefault; // modern standard
+                window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+                window.ontouchmove = preventDefault; // mobile
+                document.onkeydown = preventDefaultForScrollKeys;
+            }
+        }
+
+        function enableScroll() {
+            if (window.matchMedia("(max-width: 767px)").matches) {
+                if (window.removeEventListener) {
+                    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+                }
+                window.onmousewheel = document.onmousewheel = null;
+                window.onwheel = null;
+                window.ontouchmove = null;
+                document.onkeydown = null;
+            }
+        }
 
         // ========== SELECT ==========
 
